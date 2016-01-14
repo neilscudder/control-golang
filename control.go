c202fb8577ab58cecd868c07ae320474d6e06513
Neil Scudder <neil@neilscudder.com>
2016-01-13 22:58:41 -0800

    import mpd, changed name

--- control.go
+++ control.go
@@ -1,16 +1,16 @@
-package main
+package control
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/fhs/gompd"
+  "fmt"
+  "net/http"
+  "mpd"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
