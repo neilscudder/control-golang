fc42f9b232c02fe5c466077cc8ea0ad2a0d349c7
Neil Scudder <neil@neilscudder.com>
2016-01-13 23:08:27 -0800

    mpd skeleton

--- control.go
+++ control.go
@@ -6,8 +6,38 @@ import (
   "mpd"
 )
 
+func mpdConnect() {
+  conn, err := DialAuthenticated("tcp", "192.168.9.108:6600", "user")
+  if err != nil {
+    log.Fatalln(err)
+  }
+  defer conn.Close()
+}
+
+func mpdStatus() {
+  line := ""
+  line1 := ""
+  status, err := conn.Status()
+  if err != nil {
+    log.Fatalln(err)
+  }
+  song, err := conn.CurrentSong()
+  if err != nil {
+    log.Fatalln(err)
+  }
+  if status["state"] == "play" {
+    line1 = fmt.Sprintf("%s - %s", song["Artist"], song["Title"])
+  } else {
+    line1 = fmt.Sprintf("State: %s", status["state"])
+  }
+  if line != line1 {
+    line = line1
+    fmt.Println(line)
+  }
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+  fmt.Fprintf(w, mpdStatus())]
 }
 
 func main() {
