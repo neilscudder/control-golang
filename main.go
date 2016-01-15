097f09f6ef4bd3ed8808267580d137762c454773
Neil Scudder <neil@neilscudder.com>
2016-01-15 13:51:18 -0800

    renaming vars

--- main.go
+++ main.go
@@ -10,30 +10,24 @@ import (
 
 func mpdStatus() string {
   conn, err := mpd.DialAuthenticated("tcp", "192.168.9.108:6600", "user")
-  if err != nil {
-    log.Fatalln(err)
-  }
+  if err != nil { log.Fatalln(err) }
   defer conn.Close()
 
-  line := ""
-  line1 := ""
+  bufferedStatus := ""
+  currentStatus := ""
   status, err := conn.Status()
-  if err != nil {
-    log.Fatalln(err)
-  }
+  if err != nil { log.Fatalln(err) }
   song, err := conn.CurrentSong()
-  if err != nil {
-    log.Fatalln(err)
-  }
+  if err != nil { log.Fatalln(err) }
   if status["state"] == "play" {
-    line1 = fmt.Sprintf("%s - %s", song["Artist"], song["Title"])
+    currentStatus = fmt.Sprintf("%s - %s", song["Artist"], song["Title"])
   } else {
-    line1 = fmt.Sprintf("State: %s", status["state"])
+    currentStatus = fmt.Sprintf("State: %s", status["state"])
   }
-  if line != line1 {
-    line = line1
+  if bufferedStatus != currentStatus {
+    bufferedStatus = currentStatus
   }
-  return line
+  return bufferedStatus
 }
 
 func gui(w http.ResponseWriter, r *http.Request) {
