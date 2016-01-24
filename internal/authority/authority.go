59a548caf0363b015c86a433fc4102f81cd62169
Neil Scudder <neil@neilscudder.com>
2016-01-24 01:53:13 -0800

    whoops

---
+++ internal/authority/authority.go
@@ -0,0 +1,41 @@
+package authority
+
+import (
+  "errors"
+  "log"
+  "path/filepath"
+  "encoding/json"
+  "io/ioutil"
+  "github.com/nu7hatch/gouuid"
+)
+
+func save(kpass string,rpass string,obj map[string]string) error {
+  filename := "data/" + kpass + "." + rpass
+  byteP,ror := json.Marshal(obj); er(ror)
+  return ioutil.WriteFile(filename, byteP, 0600)
+}
+
+func Authenticate(kpass string) ([]byte,error) {
+  file,ror := filepath.Glob("data/" + kpass + ".*"); er(ror)
+  if file != nil {
+    log.Printf("Authenticated: %v", kpass)
+    return ioutil.ReadFile(file[0])
+  }
+  log.Printf("Access Denied: %v", kpass)
+  return nil,errors.New("Access Denied")
+}
+
+func Authorize(obj map[string]string) string {
+  kpass,ror := uuid.NewV4(); er(ror)
+  rpass,ror := uuid.NewV4(); er(ror)
+  k := kpass.String()
+  r := rpass.String()
+  ror = save(k,r,obj); er(ror)
+  return k
+}
+
+func er(ror error){
+  if ror != nil { log.Fatalln(ror) }
+}
+
+
