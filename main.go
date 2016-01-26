f095c95777530c3cde5e9554a2505c71889b48ed
Neil Scudder <neil@neilscudder.com>
2016-01-26 14:16:37 -0800

    Using map instead of custom struct for Params

--- main.go
+++ main.go
@@ -10,16 +10,6 @@ import (
   "github.com/neilscudder/control-golang/mpdcacher"
 )
 
-type Params struct {
-  APIURL,
-  LABEL,
-  EMAIL,
-  MPDPORT,
-  MPDHOST,
-  MPDPASS,
-  KPASS string
-}
-
 func main() {
   http.HandleFunc("/", gui)
   http.HandleFunc("/get", get)
@@ -30,7 +20,7 @@ func main() {
 }
 
 func gui(w http.ResponseWriter, r *http.Request) {
-  var p Params
+  var p map[string]string
   kpass := r.FormValue("KPASS")
   p,_ = getParams(kpass)
   t, ror := template.ParseGlob("templates/gui/*"); er(ror)
@@ -39,20 +29,15 @@ func gui(w http.ResponseWriter, r *http.Request) {
 func get(w http.ResponseWriter, r *http.Request) {
   kpass := r.FormValue("KPASS")
   p,ror := getParams(kpass); er(ror)
-  params := map[string]string {
-    "MPDHOST": p.MPDHOST,
-    "MPDPORT": p.MPDPORT,
-    "MPDPASS": p.MPDPASS,
-  }
   cmd := r.FormValue("a")
   w.Header().Set("Content-Type", "text/html")
-  u := mpdcacher.MpdStatus(cmd,params)
+  u := mpdcacher.MpdStatus(cmd,p)
   t, ror := template.ParseFiles("templates/status.html"); er(ror)
   t.Execute(w,u)
 }
 
-func getParams(kpass string) (Params,error){
-  var p Params
+func getParams(kpass string) (map[string]string,error){
+  var p map[string]string
   byteP,err := authority.Authenticate(kpass)
   if err == nil {
     err = json.Unmarshal(byteP,&p)
@@ -70,26 +55,21 @@ func setup(w http.ResponseWriter, r *http.Request) {
 }
 
 func auth(w http.ResponseWriter, r *http.Request) {
-  p := &Params{
-    APIURL: r.FormValue("APIURL"),
-    LABEL: r.FormValue("LABEL"),
-    EMAIL: r.FormValue("EMAIL"),
-    MPDPORT: r.FormValue("MPDPORT"),
-    MPDHOST: r.FormValue("MPDHOST"),
-    MPDPASS: r.FormValue("MPDPASS"),
-    KPASS: r.FormValue("KPASS"),
-  }
-  s := map[string]string {
-    "MPDHOST": p.MPDHOST,
-    "MPDPORT": p.MPDPORT,
-    "MPDPASS": p.MPDPASS,
+  p := map[string]string{
+    "APIURL": r.FormValue("APIURL"),
+    "LABEL": r.FormValue("LABEL"),
+    "EMAIL": r.FormValue("EMAIL"),
+    "MPDPORT": r.FormValue("MPDPORT"),
+    "MPDHOST": r.FormValue("MPDHOST"),
+    "MPDPASS": r.FormValue("MPDPASS"),
+    "KPASS": r.FormValue("KPASS"),
   }
   cURL := r.FormValue("GUIURL") + "/?"
-  if p.APIURL != "" { cURL += "&APIURL=" + p.APIURL }
+  if p["APIURL"] != "" { cURL += "&APIURL=" + p["APIURL"] }
   rURL := cURL
   cURL += "&KPASS="
   rURL += "&RPASS="
-  kpass,rpass := authority.Authorize(s)
+  kpass,rpass := authority.Authorize(p)
   cURL += kpass
   rURL += rpass
   u := map[string]string{
