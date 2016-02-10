e6f554abfa01be6c26db774a5d5b61289016859f
Neil Scudder <neil@neilscudder.com>
2016-02-10 01:26:49 -0800

    Finished segregating the state and status - so buttons and banner (state) can update without refreshing main window (status)

--- templates/gui/gui.js
+++ templates/gui/gui.js
@@ -38,46 +38,31 @@ function sendCmd(id) {
         animatedButtonListener()
       }
     } else {
-      var CurrentState = this.responseText;
+      var CurrentState = this.responseText
       var button = document.getElementById(id)
+      var banner = document.getElementById('BannerArea')
       if (CurrentState !== PreviousState && !isEmpty(CurrentState)) {
         state = JSON.parse(CurrentState)
         PreviousState = CurrentState
-      	if (state.BannerText) {
-      	  banner.innerHTML = state.BannerText
-      	}
-      	if (state.Random) {
-      	  if (state.Random == '0') { 
-      	    document.getElementById("random").style.backgroundColor = "#586e75"
-      	  } else {
-      	    document.getElementById("random").style.backgroundColor = "#839496"
-      	  }
-      	}
-      	if (state.Repeat) {
-      	  if (state.Repeat == '0') { 
-      	    document.getElementById("repeat").style.backgroundColor = "#586e75"
-      	  } else {
-      	    document.getElementById("repeat").style.backgroundColor = "#839496"
-      	  }
-      	}
-      	if (state.Play) {
-      	  var playSVG = document.getElementById('playsvg')
-      	  if (state.Play == 'play') {
-      	    var pausePaths = '<path fill=\"#002B36\ "d=\"M6 19h4V5H6v14zm8-14v14h4V5h-4z\"></path><path d=\"M0 0h24v24H0z\" fill=\"none\"></path>'
-      	    playsvg.innerHTML = pausePaths
-      	  } else {
-      	    var playPaths = '<path fill=\"#002B36\" d=\"M8 5v14l11-7z\" ></path><path fill=\"none\" d=\"M0 0h24v24H0z\"></path>'
-      	    playsvg.innerHTML = playPaths
-      	  }
-      	}
-        /*      
-        if (state.Volume) {
-      	  var volume = state.Volume
-      	  volume = volume * 0.01
-      	  var inverse = 1 - volume
-      	  document.getElementById("dnsvg").style.opacity = volume
-      	  document.getElementById("upsvg").style.opacity = inverse
-      	}*/
+      	banner.innerHTML = state.Banner
+    	  if (state.Random == 0) { 
+    	    document.getElementById("random").style.backgroundColor = "#586e75"
+    	  } else {
+    	    document.getElementById("random").style.backgroundColor = "#839496"
+    	  }
+    	  if (state.Repeat == 0) { 
+    	    document.getElementById("repeat").style.backgroundColor = "#586e75"
+    	  } else {
+    	    document.getElementById("repeat").style.backgroundColor = "#839496"
+    	  }
+    	  var playSVG = document.getElementById('playsvg')
+    	  if (state.Play == 'play') {
+    	    var pausePaths = '<path fill=\"#002B36\ "d=\"M6 19h4V5H6v14zm8-14v14h4V5h-4z\"></path><path d=\"M0 0h24v24H0z\" fill=\"none\"></path>'
+    	    playsvg.innerHTML = pausePaths
+    	  } else {
+    	    var playPaths = '<path fill=\"#002B36\" d=\"M8 5v14l11-7z\" ></path><path fill=\"none\" d=\"M0 0h24v24H0z\"></path>'
+    	    playsvg.innerHTML = playPaths
+    	  }
         if (button.classList.contains("pushed")) {
         	button.classList.remove('pushed')
         	button.classList.add('released')
